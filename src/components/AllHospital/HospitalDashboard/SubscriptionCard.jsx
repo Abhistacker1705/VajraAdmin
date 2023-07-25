45480676b3398b1c393796cf74d88baa189529f7
Abhijith Nair H <53036069+Abhistacker1705@users.noreply.github.com>
2023-07-25 17:09:18 +0530

    Made changes in Subscription Card

--- src/components/AllHospital/HospitalDashboard/SubscriptionCard.jsx
+++ src/components/AllHospital/HospitalDashboard/SubscriptionCard.jsx
@@ -5,58 +5,56 @@ import React from "react";
 const SubscriptionCard = () => {
   return (
     <Box
+      sx={{
+        boxShadow: "0px 0px 4px 0px #0000001F",
+        borderRadius: "20px",
+        marginTop: "20px",
+      }}
       width={{
         sm: "100%",
         xs: "90vw",
       }}>
-      <Grid
-        width="100%"
-        maxHeight="196px"
-        sx={{background: "white"}}
-        marginTop="20px"
-        borderRadius="20px"
-        border="1px solid #F7811740"
-        boxShadow="0px 0px 4px 0px #0000001F">
-        <Card sx={{borderRadius: "20px", border: "none", height: "100%"}}>
-          <CardContent
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}>
-            <Box marginTop="35px">
-              <Typography
-                sx={{
-                  fontSize: "22px",
-                  fontWeight: "500",
-                  color: "#1746A2",
-                  marginLeft: "49px",
-                }}>
-                Plus
-              </Typography>
-              <Typography>
-                <img
-                  src="/plan.svg"
-                  style={{
-                    width: "50px",
-                    height: "40px",
-                    marginLeft: "48px",
-                  }}
-                  alt=""
-                />
-              </Typography>
-              <Typography
-                sx={{
-                  fontSize: "22px",
-                  fontWeight: "500",
-                  color: "#212427",
-                }}>
-                Current Plan
-              </Typography>
-            </Box>
-          </CardContent>
-        </Card>
-      </Grid>
+      <Card sx={{borderRadius: "20px", border: "none", height: "100%"}}>
+        <CardContent
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}>
+          <Box
+            display="flex"
+            flexDirection="column"
+            gap="1rem"
+            alignItems="center">
+            <Typography
+              sx={{
+                fontSize: "22px",
+                fontWeight: "500",
+                color: "#1746A2",
+              }}>
+              Plus
+            </Typography>
+
+            <img
+              src="/plan.svg"
+              style={{
+                width: "50px",
+                height: "40px",
+              }}
+              alt=""
+            />
+
+            <Typography
+              sx={{
+                fontSize: "22px",
+                fontWeight: "500",
+                color: "#212427",
+              }}>
+              Current Plan
+            </Typography>
+          </Box>
+        </CardContent>
+      </Card>
     </Box>
   );
 };
